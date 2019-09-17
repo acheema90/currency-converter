8b1513c63e78681557a6210ac7b54edeae4a001b
Ajaypal Cheema <ajaypalster@gmail.com>
2019-09-17 17:19:56 -0400

    Updates

---
+++ precache-manifest.4557200f3e179d4d5ea0049149e5f79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c270f7622a28495a061d6b185f97b71",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "eeaff7f8971b76e96142",
+    "url": "/currency-converter/static/css/main.46fc82bc.chunk.css"
+  },
+  {
+    "revision": "85fbb4052a254ed0acab",
+    "url": "/currency-converter/static/js/2.cb4a20bd.chunk.js"
+  },
+  {
+    "revision": "eeaff7f8971b76e96142",
+    "url": "/currency-converter/static/js/main.3059d2de.chunk.js"
+  },
+  {
+    "revision": "1ff46a7cba55befff630",
+    "url": "/currency-converter/static/js/runtime~main.4b7fa448.js"
+  }
+]);
